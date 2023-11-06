90cb7cdae9831e7e2ee822e978831fe08e37340e
LucasAH9 <lucasandosilla@gmail.com>
2023-11-06 21:44:28 +0100

    Aplicacion final
    Completada pantalla de ranking y rediseño visual.
    version final entregada

--- app/src/main/java/com/example/animalcrossing/escenas/PrimeraEscena.kt
+++ app/src/main/java/com/example/animalcrossing/escenas/PrimeraEscena.kt
@@ -1,10 +1,12 @@
 package com.example.animalcrossing.escenas
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,9 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 import androidx.navigation.NavController
 import androidx.room.Room
@@ -26,6 +31,7 @@ import com.example.animalcrossing.Navegacion.NavegacionEscenas
 import com.example.animalcrossing.Data.DatabaseViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.animalcrossing.Data.BaseDeDatos
+import com.example.animalcrossing.R
 
 
 @ExperimentalMaterial3Api
@@ -45,18 +51,24 @@ fun PrimeraEscena(controladorNavegacion: NavController, db:BaseDeDatos){
 @Composable
 fun Escena1Cuerpo(controladorNavegacion: NavController, data: BaseDeDatos){
 
-
+    val miColor = Color(112,129,94,255)
 
     Column (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Animal Crossing Quiz")
+        Image(
+            painter = painterResource(id = R.drawable.animalcrossingquizzpic),
+            contentDescription = "Icono del menú" //
+        )
+        //Text(text = "Animal Crossing Quiz")
 
-        Button(onClick = { controladorNavegacion.navigate(route=NavegacionEscenas.SegundaEscena.route)}
-            ) {
-
+        Button(onClick = { controladorNavegacion.navigate(route=NavegacionEscenas.SegundaEscena.route)},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = miColor
+            )
+        ) {
             Text(text = "Jugar")
         }
         if (data.jugadorDao().getNumActivePlayer() > 0) {
@@ -66,12 +78,31 @@ fun Escena1Cuerpo(controladorNavegacion: NavController, data: BaseDeDatos){
             Button(onClick = {
                 //quitar el activeplayer
                 data.jugadorDao().setPlayerActiveState(newName = activePlayer, state = false)
-                controladorNavegacion.navigate(route = NavegacionEscenas.PrimeraEscena.route)})
+                controladorNavegacion.navigate(route = NavegacionEscenas.PrimeraEscena.route)},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = miColor
+                )
+            )
+
             {
                 Text(text = "Cerrar Sesion")
             }
+            Button(onClick = {
+                controladorNavegacion.navigate(route = NavegacionEscenas.TerceraEscena.route)},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = miColor
+                )
+            )
+            {
+                Text(text = "Datos personales y ranking")
+            }
+
         } else {
-            Button(onClick = { controladorNavegacion.navigate(route = NavegacionEscenas.IniciarSesionEscena.route) }) {
+            Button(onClick = { controladorNavegacion.navigate(route = NavegacionEscenas.IniciarSesionEscena.route) },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = miColor
+                )
+            ) {
                 Text(text = "Iniciar Sesion")
                 }
 
--- app/src/main/java/com/example/animalcrossing/escenas/TerceraEscena.kt
+++ app/src/main/java/com/example/animalcrossing/escenas/TerceraEscena.kt
@@ -1,10 +1,114 @@
 package com.example.animalcrossing.escenas
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.animalcrossing.Data.BaseDeDatos
+import com.example.animalcrossing.Navegacion.NavegacionEscenas
 
-fun TerceraEscena(controladorNavegacion: NavController){
+@ExperimentalMaterial3Api
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun TerceraEscena(controladorNavegacion: NavController, db:BaseDeDatos){
 
+    Scaffold {
+
+        Escena3Cuerpo(controladorNavegacion,db)
+    }
+}
+
+@ExperimentalMaterial3Api
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun Escena3Cuerpo(controladorNavegacion: NavController, data:BaseDeDatos) {
+    val rankingList = data.jugadorDao().getRanking()
+    val gamer = data.jugadorDao().getActivePlayerData()
+    val miColor = Color(112,129,94,255)
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Última puntuación de " + data.jugadorDao().getActivePlayer(),
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier.padding(16.dp)
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "" + gamer.lastScore + " puntos",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.padding(20.dp)
+            )
+            Button(onClick = {
+                controladorNavegacion.navigate(route = NavegacionEscenas.PrimeraEscena.route)
+            },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = miColor
+                )
+            ) {
+                Text("Salir")
+            }
+            Text(
+                text = "Ranking",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                ),
+                modifier = Modifier.padding(25.dp)
+            )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                rankingList.forEach { jugador ->
+                    Text(
+                        text = jugador.nombre +" : " + jugador.maxScore,
+                        style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
 
 
 }
